88da2978a9811b5df6fe69e20c88992289c6d9f2
Alex Elgin <sascha.elgin@gmail.com>
2019-04-18 14:23:52 +0600

    fix the issue with class arrow functions

--- index.js
+++ index.js
@@ -1,100 +1,114 @@
 const spawn = require('child_process').spawn;
 
 const callRegexps = {
-  callEstablished: /Call established: (.+)/,
-  callReceived: /Incoming call from: (\d+ )*(\S+) -/,
-  hangUp: /(.+): session closed/,
+    callEstablished: /Call established: (.+)/,
+    callReceived: /Incoming call from: (\d+ )*(\S+) -/,
+    hangUp: /(.+): session closed/,
 };
 
 const keyErrorLogs = {
-  callMuted: 'call muted',
-  callUnmuted: 'call un-muted',
-  serverConnected: 'registered successfully',
+    callMuted: 'call muted',
+    callUnmuted: 'call un-muted',
+    serverConnected: 'registered successfully',
 };
 
 class Baresip {
-  constructor(processPath, callbacks = {}) {
-    this.callbacks = {};
+    constructor(processPath, callbacks = {}) {
+        this.callbacks = {};
 
-    [
-      'serverConnected',
-      'ready',
-      'hangUp',
-      'callReceived',
-      'callEstablished',
-      'callMuted',
-      'callUnmuted',
-    ].forEach((event) => {
-      this.on(event, callbacks[event] === undefined ? () => {} : callbacks[event]);
-    });
+        [
+            'serverConnected',
+            'ready',
+            'hangUp',
+            'callReceived',
+            'callEstablished',
+            'callMuted',
+            'callUnmuted',
+        ].forEach((event) => {
+            this.on(event, callbacks[event] === undefined ? () => {} : callbacks[event]);
+        });
 
-    this.connect(processPath);
-  }
+        this.connect(processPath);
 
-  on = (event, callback) => {
-    this.callbacks[event] = callback;
-  };
+        [
+            'on',
+            'connect',
+            'hangUp',
+            'kill',
+            'reload',
+            'accept',
+            'dial',
+            'toggleCallMuted'
+        ].forEach((method) => {
+            this[method] = this[method].bind(this);
+        });
+    }
 
-  connect = (processPath) => {
-    this.processPath = processPath;
-    this.baresip = spawn(this.processPath);
-    this.baresip.stdin.setEncoding('utf-8');
+    on(event, callback) {
+        this.callbacks[event] = callback;
+    }
 
-    this.baresip.stdout.on('data', (data) => {
-      const parsedData = `${data}`;
+    connect(processPath) {
+        this.processPath = processPath;
+        this.baresip = spawn(this.processPath);
+        this.baresip.stdin.setEncoding('utf-8');
 
-      if (parsedData.includes('baresip is ready')) {
-        this.callbacks.ready();
-      }
+        this.baresip.stdout.on('data', (data) => {
+            const parsedData = `${data}`;
 
-      Object.keys(callRegexps).forEach((event) => {
-        const matches = parsedData.match(callRegexps[event]);
+            if (parsedData.includes('baresip is ready')) {
+                this.callbacks.ready();
+            }
 
-        if ((matches !== null) && (matches.length > 0)) {
-          this.callbacks[event](matches[matches.length - 1].replace('SIP ', ''));
-        }
-      });
+            Object.keys(callRegexps).forEach((event) => {
+                const matches = parsedData.match(callRegexps[event]);
 
-      console.log(parsedData);
-    });
+                if ((matches !== null) && (matches.length > 0)) {
+                    this.callbacks[event](matches[matches.length - 1].replace('SIP ', ''));
+                }
+            });
 
-    this.baresip.stderr.on('data', (data) => {
-      const parsedData = `${data}`;
+            console.log(parsedData);
+        });
 
-      Object.keys(keyErrorLogs).forEach((event) => {
-        if (parsedData.includes(keyErrorLogs[event])) {
-          this.callbacks[event]();
-        }
-      });
+        this.baresip.stderr.on('data', (data) => {
+            const parsedData = `${data}`;
 
-      console.error(parsedData);
-    });
-  };
+            Object.keys(keyErrorLogs).forEach((event) => {
+                if (parsedData.includes(keyErrorLogs[event])) {
+                    this.callbacks[event]();
+                }
+            });
 
-  kill = () => {
-    this.baresip.kill();
-  };
+            console.error(parsedData);
+        });
+    }
 
-  reload = () => {
-    this.kill();
-    this.connect(this.processPath);
-  };
+    kill() {
+        this.baresip.kill();
+    }
 
-  accept = () => {
-    this.baresip.stdin.write('/accept\n');
-  };
+    reload() {
+        this.kill();
+        this.connect(this.processPath);
+    }
 
-  dial = (number) => {
-    this.baresip.stdin.write(`/dial ${number}\n`);
-  };
+    accept() {
+        this.baresip.stdin.write('/accept\n');
+    }
 
-  hangUp = () => {
-    this.baresip.stdin.write('/hangup\n');
-  };
+    dial(phoneNumber) {
+        this.baresip.stdin.write(`/dial ${phoneNumber}\n`);
+    }
 
-  toggleCallMuted = () => {
-    this.baresip.stdin.write('/mute\n');
-  };
+    hangUp() {
+        this.baresip.stdin.write('/hangup\n');
+    }
+
+    toggleCallMuted() {
+        this.baresip.stdin.write('/mute\n');
+    }
 }
 
 module.exports = Baresip;
+
